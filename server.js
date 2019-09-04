6f3145c59073a2aba59aee5f55d24785eacf69fd
Matthew Klundt <matt2@terribleproductions.com>
2019-09-04 13:53:50 -0500

    1.3.12, Fix moving of proxy.log to dated file

--- server.js
+++ server.js
@@ -25,12 +25,18 @@ if (config.folders.logs) {
 
 	// if proxy.log exists, and is not empty append to today's date
 	if (fs.existsSync(config.folders.logs+"proxy.log")) {
-		var file = fs.readFileSync(config.folders.logs+'proxy.log', 'utf8');
-		if (file) {
-			fs.appendFileSync(config.folders.logs + moment().format('YYYYMMDD') + "_proxy.log", fs.readFileSync(config.folders.logs+"proxy.log"));
-			// fs.unlinkSync(config.folders.logs+"/proxy.log");
-			fs.writeFileSync(config.folders.logs+'proxy.log', "");
+		try {
+			var command = 'cat '+config.folders.logs+'proxy.log >> '+config.folders.logs + moment().format('YYYYMMDD') + '_proxy.log';
+			var resp = execSync(command, {encoding:"utf8"});
+		} catch(err) {
+			logEvent(2, "Proxy.log error", err);
 		}
+		// var file = fs.readFileSync(config.folders.logs+'proxy.log', 'utf8');
+		// if (file) {
+		// 	fs.appendFileSync(config.folders.logs + moment().format('YYYYMMDD') + "_proxy.log", fs.readFileSync(config.folders.logs+"proxy.log"));
+		// 	// fs.unlinkSync(config.folders.logs+"/proxy.log");
+		fs.writeFileSync(config.folders.logs+'proxy.log', "");
+		// }
 	}
 }
 
@@ -80,8 +86,8 @@ if (fs.existsSync(config.base_dir+'/'+config.folders.proxy+'/state.json')) {
 
 _.each(config.service_versions, function (version, service) {
 	try {
-	    var service_config  = require(config.base_dir + '/' + config.folders[service] + '/config.js');
-	    config.service_versions[service] = service_config.version;
+    var service_config  = require(config.base_dir + '/' + config.folders[service] + '/config.js');
+    config.service_versions[service] = service_config.version;
 		var command = 'cd '+config.base_dir+'/'+config.folders[service]+' && git rev-parse --abbrev-ref HEAD';
 		var resp = execSync(command, {encoding:"utf8"});
 		config.service_branches[service] = resp.trim();
